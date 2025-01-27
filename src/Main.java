7c649bd692c0cd08bf8a2ce8f98a20aaff1ced1e
yDiffraction <144171960+yDiffraction@users.noreply.github.com>
2025-01-27 13:04:22 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import OutputGenerator.OutputGenerator;
+import PatternDetermination.PatternDeterminatiom;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        PatternDetermination patternGen = new PatternDetermination();
+        OutputGenerator OutputGen = new OutputGenerator();
     }
 }
