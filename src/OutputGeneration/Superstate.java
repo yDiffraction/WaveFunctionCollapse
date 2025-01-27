dad31f7587d24eda61c09beca45cfde0e270854a
yDiffraction <144171960+yDiffraction@users.noreply.github.com>
2025-01-27 13:22:33 +0100

    Update Superstate.java

--- src/OutputGeneration/Superstate.java
+++ src/OutputGeneration/Superstate.java
@@ -2,7 +2,7 @@ public class Superstate {
   boolean[] possible_patterns;
   boolean[] possible_colors;
   
-  public Superstate() {
-    this;
+  public Superstate(int num_patterns, int num_colors) {
+    this.possible_patterns = new boolean[num_patterns]();
   }
 }
