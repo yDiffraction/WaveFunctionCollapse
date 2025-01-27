4a1bb38954462a3e77ab6b3e0e014d862e4dfdf8
yDiffraction <144171960+yDiffraction@users.noreply.github.com>
2025-01-27 13:20:51 +0100

    Update GeneratorMain.java

--- src/OutputGeneration/GeneratorMain.java
+++ src/OutputGeneration/GeneratorMain.java
@@ -2,9 +2,11 @@ import PatternDetermination.Pattern;
 
 public class GeneratorMain {
   Patterns[] patterns;
+  Set<Int> colors;
   
-  public GeneratorMain(Patterns[] patterns) {
+  public GeneratorMain(Patterns[] patterns, Set<Int> colors) {
     System.out.println("Generator started");
     this.patterns = patterns;
+    this.colors = colors;
   }
 }
