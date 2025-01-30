da015834818c596d4d3cba12e2387fa616cb612e
Nilspferd <59707754+Sinx07@users.noreply.github.com>
2025-01-30 10:51:00 +0100

    Update PatternDetermination.java

--- src/PatternDetermination/PatternDetermination.java
+++ src/PatternDetermination/PatternDetermination.java
@@ -1,12 +1,26 @@
 import java.awt.*;
 import java.io.*;
 public class PatternDetermination {
+  public Set<Integer> allcolors = new HashSet<Integer>();
+  Pattern[] patterns = new Pattern[];
+  File file = new File(“WaveFunctionCollapse/src/PatternDetermination/Test.png”);
+  
   public PatternDetermination() {
     // Bild laden
-    File file = new File(“WaveFunctionCollapse/src/PatternDetermination/Test.png”);
     BufferedImage image = ImageIO.read(file);
-    height = image.getHeight();
-    width = image.getWidth();
     
+    // allen farben einen wert zuweisen
+    for (int h = 0; h < image.getHeight(); h++) {
+			for int w = 0; w < image.getWidth(); w++) {
+				allcolors.add(getRGB(w,h));
+      }
+    }
+    
+    // alle felder mit mindestens einem abstand von 1 zum rand abgehen
+    for (int h = 1; h < image.getHeight()-1; h++) {
+      for (int w = 1; w < image.getWidth()-1; w++) {
+        patterns.push(new Pattern());
+      }
+    }
   }
 }
