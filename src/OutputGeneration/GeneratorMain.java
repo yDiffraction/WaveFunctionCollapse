5af32b805063b19277623dbb0ad25cf35aa7c308
yDiffraction <144171960+yDiffraction@users.noreply.github.com>
2025-01-30 10:28:08 +0100

    Update GeneratorMain.java

--- src/OutputGeneration/GeneratorMain.java
+++ src/OutputGeneration/GeneratorMain.java
@@ -1,12 +1,34 @@
 import PatternDetermination.Pattern;
+import Superstate;
 
 public class GeneratorMain {
   Patterns[] patterns;
   Set<Int> colors;
+  Superstate[][] map;
   
-  public GeneratorMain(Patterns[] patterns, Set<Int> colors) {
+  public GeneratorMain(Patterns[] patterns, Set<Int> colors, int width, int height) {
     System.out.println("Generator started");
     this.patterns = patterns;
     this.colors = colors;
+    this.map = new Superstate[height][width](patterns.length, colors.length);
   }
+
+  public void run() {
+    while (True) {
+      return;
+    }
+
+  private void collapse() {
+    Superstate minState;
+    int minPossibilities = colors.length;
+    for (Superstate[] x : map) {
+      for (Superstate y : x) {
+        if (y.getNumberOfPossibilities() <= minPossibilities) {
+          minState = y;
+          minPossibilities = y.getNumberOfPossibilities();
+        }
+      }
+    }
+
+    minState.collapse();
 }
