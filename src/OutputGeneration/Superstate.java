d832282c5dfb4f4ea001271ae8f35061ca0e4289
yDiffraction <144171960+yDiffraction@users.noreply.github.com>
2025-01-30 10:06:29 +0100

    Update Superstate.java

--- src/OutputGeneration/Superstate.java
+++ src/OutputGeneration/Superstate.java
@@ -3,6 +3,13 @@ public class Superstate {
   boolean[] possible_colors;
   
   public Superstate(int num_patterns, int num_colors) {
+    //initialize possible_patterns and possible_colors
     this.possible_patterns = new boolean[num_patterns]();
+    this.possible_colors = new boolean[num_colors]();
+    for (int i=0;i<num_patterns;i++) {
+      possible_patterns[i] = 1;
+    }for (int i=0;i<num_colors;i++) {
+      possible_colors[i] = 1;
+    }
   }
 }
