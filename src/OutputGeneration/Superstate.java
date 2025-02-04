1ac26b26ad19affbef52b1ec5588cebe011836e6
balu <hobby835@web.de>
2025-02-04 20:14:41 +0100

    a

--- src/OutputGeneration/Superstate.java
+++ src/OutputGeneration/Superstate.java
@@ -1,5 +1,7 @@
 package OutputGeneration;
 
+import java.util.Arrays;
+
 public class Superstate {
   boolean[] possible_patterns;
   boolean[] possible_colors;
@@ -29,6 +31,29 @@ public class Superstate {
 
   //collapses the pixel. runs a recursion for collapse of nearby pixels after
   public void collapse() {
+    //choose random color for the array
+    int num_possibilities = 0;
+    for (boolean i : possible_colors) {
+      if (i) {
+        num_possibilities++;
+      }
+    }
+
+    int rand = (int) (Math.random() * num_possibilities);
+    for (int i = 0; i < possible_colors.length; i++) {
+      if (!possible_colors[i]) {
+        continue;
+      }
+      if (rand == 0) {
+        Arrays.fill(possible_colors, false);
+        possible_colors[i] = true;
+        break;
+      }
+      rand--;
+    }
+  }
+
+  public void update_state() {
     return;
   }
 }
