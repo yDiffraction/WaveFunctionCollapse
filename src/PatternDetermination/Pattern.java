0d470649432b85f0c9f26482b75aee40302df28a
Nilspferd <59707754+Sinx07@users.noreply.github.com>
2025-02-06 11:15:18 +0100

    Add files via upload

--- src/PatternDetermination/Pattern.java
+++ src/PatternDetermination/Pattern.java
@@ -4,9 +4,21 @@ public class Pattern {
   public int[][] map; // int are colores(they are spanish!)
   public float weight;
 
-  public Pattern(int[][] map, float weight) {
-    this.map = map;
-    this.weight = weight;
+  public Pattern(int pattern_size) {
+    map = new int[pattern_size][pattern_size];
+    weight = 1;
+  }
+  public Pattern(int[][] map, int weight) {
+	 this.map = map;
+	 this.weight = weight;
+	 }
+  
+  public void setPixelColor(int x, int y, int colorID) {
+	  map[x][y] = colorID;
+  }
+  public void addWeight() {
+	  weight++;
+	 
   }
 
   public int getSize() {
